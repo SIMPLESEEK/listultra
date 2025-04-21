08d5e82b6a73b64b241b9a5ea4e9fa94932fcce8
simpleseek <liuyanjiang@gmail.com>
2025-04-21 18:03:00 +0800

    Debug: Add log to NextAuth route file init

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,8 @@ import bcrypt from 'bcryptjs';
 import connectDB from '@/lib/mongodb';
 import User from '@/models/User';
 
+console.log('[Route Init] Loading /api/auth/[...nextauth]/route.ts');
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }; 
